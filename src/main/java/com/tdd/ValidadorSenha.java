71672c9d927058e2ab2e2c870b24ffeddba84eaa
LucasMKS <lucas.mar17ques@gmail.com>
2025-05-20 15:17:19 -0300

    first commit

---
+++ src/main/java/com/tdd/ValidadorSenha.java
@@ -0,0 +1,11 @@
+package com.tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidadorSenha {
+     public static List<String> validar(String senha) {
+        // passo 1: método vazio
+        return new ArrayList<>();
+    }
+}
