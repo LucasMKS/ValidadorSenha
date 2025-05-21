7cf73d7a74eab4bab7884e21f255175f0e62f382
Willian Douglas de Almeida Alexandre <88801867+Willian6@users.noreply.github.com>
2025-05-21 16:48:18 +0000

    feat: Implementa validação de senha com regras específicas

--- src/main/java/com/tdd/ValidadorSenha.java
+++ src/main/java/com/tdd/ValidadorSenha.java
@@ -5,7 +5,31 @@ import java.util.List;
 
 public class ValidadorSenha {
      public static List<String> validar(String senha) {
-        // passo 1: método vazio
-        return new ArrayList<>();
+        List<String> erros = new ArrayList<>();
+
+        // 1) comprimento mínimo
+        if (senha == null || senha.length() < 8) {
+            erros.add("A senha deve ter pelo menos 8 caracteres");
+        }
+
+        // 2) pelo menos 2 dígitos
+        long countDigitos = (senha == null)
+                ? 0
+                : senha.chars().filter(Character::isDigit).count();
+        if (countDigitos < 2) {
+            erros.add("A senha deve conter pelo menos 2 dígitos");
+        }
+
+        // 3) ao menos 1 letra maiúscula
+        if (senha == null || !senha.chars().anyMatch(Character::isUpperCase)) {
+            erros.add("A senha deve conter pelo menos uma letra maiuscula");
+        }
+
+        // 4) ao menos 1 caractere especial
+        if (senha == null || !senha.matches(".[^A-Za-z0-9].")) {
+            erros.add("A senha deve conter pelo menos um caractere especial");
+        }
+
+        return erros;
     }
 }
