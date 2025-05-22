4a6b143628827e3491c0ed30c656c4481f5bca32
LucasMKS <lucas.mar17ques@gmail.com>
2025-05-22 10:23:41 -0300

    fix: corrige importação da classe assertEquals no teste ValidadorSenhaTest

--- src/test/java/com/tdd/ValidadorSenhaTest.java
+++ src/test/java/com/tdd/ValidadorSenhaTest.java
@@ -2,6 +2,8 @@ package com.tdd;
 
 import org.junit.jupiter.api.Test;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ValidadorSenhaTest {
